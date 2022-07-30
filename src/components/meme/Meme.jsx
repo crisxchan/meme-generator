a9bca4a4826572b7009ad15cadef49d1bda93441
crisxchan <chrisgonzales.online@gmail.com>
2022-07-30 23:48:04 +0800

    Create working random meme image generator

--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -1,13 +1,24 @@
 import './meme.css'
+import {useState} from 'react'
+import memesData from '../../memesData'
 
 export default function Meme() {
+    const [memeUrl, setMemeUrl] = useState('')
+    const memesArray = memesData.data.memes 
+
+    function getMemeData() {             
+        const randomIndex = Math.round(Math.random() * memesArray.length)
+        setMemeUrl(memesArray[randomIndex].url)
+    }
+
     return (
         <section id="meme">
             <div id="input">
                 <input type={"text"} placeholder={"Top text"} />
                 <input type={"text"} placeholder={"Bottom text"}/>    
             </div>
-            <button>Get a new meme image</button>
+            <button onClick={getMemeData}>Get a new meme image</button>
+            <img src={memeUrl} />
         </section>
     )
 }
